154fcbaa799009261b4978d62c9eabfc95cd76b8
Максимилиан <max4tim@bk.ru>
2023-03-07 22:30:40 +0300

    Con1 B

--- contest1/B.001.c
+++ contest1/B.001.c
@@ -1,22 +1,49 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
+#include <math.h>
 
-int comparatorInt(const void* a, const void* b);
-
-int main() {
-    
-
-    return 0;
+int min(const int a, const int b) {
+    if (a < b) return a;
+    return b;
 }
 
-int comparatorInt(const void* a, const void* b) {
-    int arg1 = *(const int*)a;
-    int arg2 = *(const int*)b;
+bool BinarySearch(const int* begin, const int* end, int target) {
+    size_t delta = ((end - begin) ) >> 1;
+    if (delta == 0)
+        return *begin == target;
 
-    if (arg1 < arg2) return -1;
-    if (arg1 > arg2) return 1;
+    const int* mid = begin + delta;
+
+    if (*mid > target) {
+        return BinarySearch(begin, mid, target);
+    } else {
+        return BinarySearch(mid, end, target);
+    }
+
+    return false;
+}
+
+int main() {
+    int n = 0, q = 0;
+    int arr[110000] = { 0 };
+    int i = 0, j = 0, t = 0;
+
+    if (scanf("%d", &n) != 1) return 2;
+    for (int i = 0; i < n; ++i) {
+        if (scanf("%d", arr + i) != 1) return 2;
+    }
+    if (scanf("%d", &q) != 1) return 2;
+
+    while (q--) {
+        if (scanf("%d %d %d", &i, &j, &t) != 3) return 2;
+
+        if (BinarySearch(arr + i, arr + j, t)) {
+            printf("YES\n");
+        } else {
+            printf("NO\n");
+        }
+    }
+
     return 0;
-
-    // return (arg1 > arg2) - (arg1 < arg2); // possible shortcut
-    // return arg1 - arg2; // erroneous shortcut (fails if INT_MIN is present)
 }
